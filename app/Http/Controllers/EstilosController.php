dd5c1cf46880160bbe2f9ec9780ee26fef55716e
Adrián Serrano <adriansb3105@gmail.com>
2020-04-24 15:16:36 -0600

    Se realiza el query a la base de datos en el endpoint del API

--- app/Http/Controllers/EstilosController.php
+++ app/Http/Controllers/EstilosController.php
@@ -2,27 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Euclidean;
-use NaiveBayes;
-use Session;
 
 class EstilosController extends Controller
 {
-    public function getStyles(Request $request)
+    public function getEstilos()
     {
-        $styles = DB::table('estilos')->select('ec', 'or', 'ca', 'ea', 'style')->get();
-        $min = 1000;
-
-        $ec = $request->input('ec');
-        $or = $request->input('or');
-        $ca = $request->input('ca');
-        $ea = $request->input('ea');
-        $algorithm = $request->input('algorithm');
-
-        $vectorX = [$ec, $or, $ca, $ea];
-
-        return Euclidean::euclidean($vectorX, $styles);
+        $estilos = DB::table('estilos')->select('ca', 'ec', 'ea', 'ro', 'estilo')->get();
+        //$select = DB::select('select ca, ec, ea, ro, estilo from estilos');
+        
+        return $estilos;
     }
 }
